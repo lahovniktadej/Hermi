8a0286472f60968ac17a5140064c718e33ec7a15
Tjan Ljubesek <tjan.ljubesek@student.um.si>
2021-05-23 13:30:21 +0200

    feat: Updated demo project

---
+++ Microservices/demo/src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+public class UserDto {
+    private String name;
+    private String email;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
