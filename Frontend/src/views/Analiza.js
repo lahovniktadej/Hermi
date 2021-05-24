dc2bb92ebbe21fe47191fd7706e549482bf2f099
ivanalav <ivana.lavric@student.um.si>
2021-05-24 20:27:30 +0200

    feat: analiza - delujoče filtriranje

--- Frontend/src/views/Analiza.js
+++ Frontend/src/views/Analiza.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {
     Badge,
@@ -33,11 +33,11 @@ import Header from 'components/Headers/Header';
 
 const data = [
     {
-        objekt: "objekt",
-        datum: "22.05.2021",
+        objekt: "objekt1",
+        datum: "2021-05-22",
         avto: "avto",
-        sofer: "priimekSoferja",
-        delavci: "priimek1, priimek2",
+        sofer: "delavec1",
+        delavci: ["delavec2", "delavec3"],
         start: "6:25",
         pricetekDela: "8:10",
         konecDela: "13:55",
@@ -51,11 +51,11 @@ const data = [
 
     },
     {
-        objekt: "objekt",
-        datum: "22.05.2021",
+        objekt: "objekt3",
+        datum: "2021-05-22",
         avto: "avto",
-        sofer: "priimekSoferja",
-        delavci: "priimek1, priimek2",
+        sofer: "delavec1",
+        delavci:  ["delavec3"],
         start: "6:25",
         pricetekDela: "8:10",
         konecDela: "13:55",
@@ -69,11 +69,11 @@ const data = [
 
     },
     {
-        objekt: "objekt",
-        datum: "22.05.2021",
+        objekt: "objekt2",
+        datum: "2020-05-22",
         avto: "avto",
-        sofer: "priimekSoferja",
-        delavci: "priimek1, priimek2",
+        sofer: "delavec1",
+        delavci:  ["delavec2", "delavec3"],
         start: "6:25",
         pricetekDela: "8:10",
         konecDela: "13:55",
@@ -86,11 +86,11 @@ const data = [
         brutoMontaza: 25
     },
     {
-        objekt: "objekt",
-        datum: "22.05.2021",
+        objekt: "objekt1",
+        datum: "2021-05-22",
         avto: "avto",
-        sofer: "priimekSoferja",
-        delavci: "priimek1, priimek2",
+        sofer: "delavec1",
+        delavci:  ["delavec2"],
         start: "6:25",
         pricetekDela: "8:10",
         konecDela: "13:55",
@@ -104,11 +104,11 @@ const data = [
 
     },
     {
-        objekt: "objekt",
-        datum: "22.05.2021",
+        objekt: "objekt2",
+        datum: "2021-05-22",
         avto: "avto",
-        sofer: "priimekSoferja",
-        delavci: "priimek1, priimek2",
+        sofer: "delavec1",
+        delavci: ["delavec2"],
         start: "6:25",
         pricetekDela: "8:10",
         konecDela: "13:55",
@@ -125,12 +125,74 @@ const data = [
 
 function Analiza() {
 
+    const [vsiPodatki, setVsiPodatki] = useState(data);
+    const [filtrirani, setFiltriran] = useState(data);
+
+    
+    const [delavci, setDelavci] = useState(["delavec1", "delavec2", "delavec3"]);
+    const [objekti, setObjekti] = useState(["objekt1", "objekt2", "objekt3"]);
+
+    const [obdobjeOD, setObdobjeOD] = useState();
+    const [obdobjeDO, setObdobjeDO] = useState();
+    const [iskanObjekt, setObjekt] = useState(objekti[0]);
+    const [delavec, setDelavec] = useState(delavci[0]);
+    const [aktiven, setAktiven] = useState(false); 
+    const [koncan, setKoncan] = useState(false);
+
+    const [obdobjeChecked, setObdobjeChecked] = useState(false);
+    const [objektChecked, setObjektChecked] = useState(false);
+    const [delavecChecked, setDelavecChecked] = useState(false);
+    const [statusChecked, setStatusChecked] = useState(false);
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        let iskaniPodatki = vsiPodatki;
+
+        if(obdobjeChecked)
+            iskaniPodatki = iskaniPodatki.filter((podatek) => ( new Date(obdobjeOD) <= new Date(podatek.datum) && new Date(obdobjeDO) >= new Date(podatek.datum)) == true); 
+        
+        console.log(iskaniPodatki);         
+        if(delavecChecked){
+            iskaniPodatki = iskaniPodatki.filter((podatek) => podatek.sofer == delavec);
+            let falseDelavci = vsiPodatki.filter((podatek) => (podatek.delavci.filter((iskanDelavec)=>{ 
+                return (iskanDelavec==delavec);
+            })
+            ) == false);
+            let iskaniDelavci = vsiPodatki.filter((podatki) => !falseDelavci.includes(podatki));
+
+            iskaniPodatki = iskaniPodatki.concat(iskaniDelavci);
+        }
+        if(objektChecked)
+            iskaniPodatki = iskaniPodatki.filter((podatek) => (podatek.objekt == iskanObjekt));
+
+        if(statusChecked){
+            if(aktiven)
+                iskaniPodatki = iskaniPodatki.filter((podatek) => podatek.status == "aktiven");
+            else if(koncan)
+                iskaniPodatki = iskaniPodatki.filter((podatek) => podatek.status == "končan");
+        }
+        
+        setFiltriran(iskaniPodatki);
+        setKoncan(false);
+        setAktiven(false);
+    }
 
     const pridobiStatus = (status) => {
         if(status == "aktiven")
             return "text-red";
         else
             return "text-green";
+    }
+    const pridobiCas = (vrsta) => {
+        let cas = [];
+        let delavec;
+        if(vrsta=="netoMontaza")
+            cas = filtrirani.map((data) => data.netoMontaza);
+        else if(vrsta=="brutoMontaza")
+            cas = filtrirani.map((data) => data.brutoMontaza);
+        let sestevek = cas.reduce((a, b) => a + b, 0);
+        return sestevek;
     }
 
     const tableRow = (el) => {
@@ -156,7 +218,7 @@ function Analiza() {
                 <td>{el.datum}</td>
                 <td>{el.avto}</td>
                 <td>{el.sofer}</td>
-                <td>{el.delavci}</td>
+                <td>{el.delavci.map((delavec) => { return (<div>{delavec}</div>) })}</td>
                 <td>{el.start}</td>
                 <td>{el.pricetekDela}</td>
                 <td>{el.konecDela}</td>
@@ -177,7 +239,7 @@ function Analiza() {
             <Container className="mt--7" fluid>
                 <Row>
                     <Col className="md-4">
-                    <Form role="form">   
+                    <Form role="form" onSubmit={handleSubmit}>   
                         <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">                            
                             FILTER <span class="ni ni-bold-down"></span>
                         </a>
@@ -185,7 +247,7 @@ function Analiza() {
                             <div class="card card-body">
                             <FormGroup check>
                                         <label className="h3">
-                                            <Input type="checkbox" name="filter"/>
+                                            <Input type="checkbox" name="filter" defaultChecked={obdobjeChecked} onChange={e => setObdobjeChecked(e.target.checked)}/>
                                               Obdobje:
                                         </label>
                                     </FormGroup>
@@ -198,6 +260,7 @@ function Analiza() {
                                             id="input-od"
                                             className="form-control-alternative"
                                             type="date"
+                                            onChange={e => setObdobjeOD(e.target.valueAsDate)}
                                         /> 
                             </FormGroup>               
                             <FormGroup>
@@ -209,11 +272,12 @@ function Analiza() {
                                     id="input-do"
                                     className="form-control-alternative"
                                     type="date"
+                                    onChange={e => setObdobjeDO(e.target.valueAsDate)}
                                 /> 
                             </FormGroup>
                             <FormGroup check>
                                 <label className="h3">
-                                    <Input type="checkbox" name="filter"/>
+                                    <Input type="checkbox" name="filter" defaultChecked={objektChecked} onChange={e => setObjektChecked(e.target.checked)}/>
                                         Objekt:
                                 </label>
                             </FormGroup>
@@ -221,16 +285,21 @@ function Analiza() {
                                 <Input  
                                     id="input-date"
                                     className="form-control-alternative"
-                                    type="select"
+                                    type="select" 
+                                    onChange={e => setObjekt(e.target.value)}
+
                                 >
-                                    <option>objekt1</option>
-                                    <option>objekt2</option>
-                                    <option>objekt3</option>
+                                    {objekti.map((objekt) => {
+                                            return(
+                                                <option>{objekt}</option>
+                                            );
+                                        }
+                                    )}
                                 </Input>
                             </FormGroup>
                             <FormGroup check>
                                 <label className="h3">
-                                    <Input type="checkbox" name="filter"/>
+                                    <Input type="checkbox" name="filter" defaultChecked={delavecChecked} onChange={e => setDelavecChecked(e.target.checked)}/>
                                         Delavec:
                                 </label>
                             </FormGroup>
@@ -240,31 +309,36 @@ function Analiza() {
                                     id="input-date"
                                     className="form-control-alternative"
                                     type="select"
+                                    onChange={e => setDelavec(e.target.value)}
                                 >
-                                    <option>delavec1</option>
-                                    <option>delavec2</option>
-                                    <option>delavec3</option>
+                                    {delavci.map((delavec) => {
+                                            return(
+                                                <option>{delavec}</option>
+                                            );
+                                        }
+                                    )}
                                 </Input>
                             </FormGroup>
                             <FormGroup check>
                                 <label className="h3">
-                                    <Input type="checkbox" name="filter"/>
+                                    <Input type="checkbox" name="filter" defaultChecked={statusChecked} onClick={e => setStatusChecked(e.target.checked)}/>
                                         Status:
                                 </label>
                             </FormGroup>
+
                             <FormGroup check>
                                 <label>
-                                    <Input type="radio" name="status"/>
+                                    <Input type="radio" name="status" checked={aktiven}  onChange={e => setAktiven(e.target.checked)}/>
                                         Aktiven
                                     </label>
                                 </FormGroup>
                                 <FormGroup check>
                                 <label>
-                                    <Input type="radio" name="status" checked/>
+                                    <Input type="radio" name="status" checked={koncan} onChange={e => setKoncan(e.target.checked)}/>
                                         Končan
-                                    </label>
+                                </label>
                             </FormGroup>
-                            <Button color="primary" type="button">Filtriraj</Button>
+                            <Button color="primary" type="submit">Filtriraj</Button>
                             </div>
                         </div>
                     </Form>
@@ -297,13 +371,24 @@ function Analiza() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {data.map((el) => tableRow(el))}
+                                    {filtrirani.map((el) => tableRow(el))}
                                 </tbody>
                             </Table>
+                        </Card><br/>
+                        <Card className="shadow">
+                            <CardHeader>
+                                <h3>Skupni časi</h3>
+                            </CardHeader>
+                            <CardBody>
+                            Neto čas montaže: <b> {pridobiCas("netoMontaza")}</b><br/>
+                            Bruto čas montaže: <b>{pridobiCas("brutoMontaza")}</b>
+                            </CardBody>
                         </Card><br/>
                         <Form role="form">
                            <Button color="primary" type="button">Shrani</Button>
                         </Form>
+
+                       
             </Container>
         </>
     );
