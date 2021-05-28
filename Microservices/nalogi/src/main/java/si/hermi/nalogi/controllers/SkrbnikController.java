7bcdee40d693a9c56643238421fb69ea1b76a877
Tjan <tljubesek@gmail.com>
2021-05-28 08:56:30 +0000

    fix: Fixed nameing

---
+++ Microservices/nalogi/src/main/java/si/hermi/nalogi/controllers/SkrbnikController.java
@@ -0,0 +1,60 @@
+package si.hermi.nalogi.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import si.hermi.nalogi.dto.SkrbnikDto;
+import si.hermi.nalogi.repositories.SkrbnikRepository;
+import si.hermi.nalogi.vao.Skrbnik;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/skrbnik")
+public class SkrbnikController {
+    @Autowired
+    private SkrbnikRepository skrbnikRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping()public @ResponseBody Iterable<Skrbnik> all() {
+        return skrbnikRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public @ResponseBody
+    ResponseEntity getSkrbnik(@PathVariable int id) {
+        Optional<Skrbnik> skrbnikOpt = skrbnikRepository.findById(id);
+        if (skrbnikOpt.isPresent()) {
+            SkrbnikDto skrbnikDto = modelMapper.map(skrbnikOpt.get(), SkrbnikDto.class);
+            return ResponseEntity.ok(skrbnikDto);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping()
+    public @ResponseBody ResponseEntity addSkrbnik(@RequestBody SkrbnikDto skrbnikDto) {
+        Skrbnik skrbnik = modelMapper.map(skrbnikDto, Skrbnik.class);
+        skrbnikRepository.save(skrbnik);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody ResponseEntity updateSkrbnik(@PathVariable int id, @RequestBody SkrbnikDto skrbnikDto) {
+        Optional<Skrbnik> skrbnikOpt = skrbnikRepository.findById(id);
+        if (skrbnikOpt.isPresent()) {
+            Skrbnik skrbnik = skrbnikOpt.get();
+            modelMapper.map(skrbnikDto, skrbnik);
+            skrbnikRepository.save(skrbnik);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody ResponseEntity deleteSkrbnik(@PathVariable int id) {
+        skrbnikRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
