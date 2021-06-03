4d332f326a3093d7c00b4bcfff4704ac241ff1b5
ivanalav <ivana.lavric@student.um.si>
2021-06-03 21:47:47 +0200

    fix: excel export

--- Frontend/src/views/ExcelExport.js
+++ Frontend/src/views/ExcelExport.js
@@ -10,13 +10,10 @@ import {
     ModalBody,
 } from "reactstrap";
 
-import ReactHTMLTableToExcel from 'react-html-table-to-excel';
-
 
 function ExcelExport(props) {
 
     const [modal, setModal] = useState();
-    const [modalBody, setModalBody] = useState();
     const toggle = () => setModal(!modal);
 
     const pridobiStatus = (status) => {
@@ -28,8 +25,30 @@ function ExcelExport(props) {
     
     const handleBody = () => {
         toggle();
-        setModalBody(excel());
     }
+
+    function excel(id){
+        let downloadLink;
+        let dataType = 'data:text/csv; charset=utf-8,';
+        let tableSelect = document.getElementById(id);
+        let tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+        let filename = 'analiza.xls';
+        let universalBOM = "\uFEFF";
+        
+        downloadLink = document.createElement("a");
+        
+        document.body.appendChild(downloadLink);
+        
+        if(navigator.msSaveOrOpenBlob){
+            let blob = new Blob(["\uFEFF", tableHTML], {encoding:"UTF-8",type:"text/plain;charset=UTF-8"});
+            navigator.msSaveOrOpenBlob(blob, filename);
+        }else{
+            downloadLink.href = 'data:' + dataType + (universalBOM+tableHTML);
+            downloadLink.download = filename;
+            downloadLink.click();
+        }
+    }
+
     const tableRow = (el) => {
         return (
             <tr>
@@ -57,62 +76,53 @@ function ExcelExport(props) {
      
         );
     };
-    const excel = () => {
-        return (
-            <>
-                <Table className="align-items-center table-flush text-center" responsive id="table">
-                <thead className="thead-light">
-                <tr>
-                    <th scope="col">Status</th>
-                    <th scope="col">Šifra</th> 
-                    <th scope="col">Objekt</th>
-                    <th scope="col">Datum</th>
-                    <th scope="col">Avto</th>
-                    <th scope="col">Šofer</th>
-                    <th scope="col">Delavci</th>
-                    <th scope="col">START</th>
-                    <th scope="col">Pričetek</th>
-                    <th scope="col">Konec</th>
-                    <th scope="col">PRIHOD</th>
-                    <th scope="col">NETO čas delavca</th>
-                    <th scope="col">Odsotnost šoferja</th>
-                    <th scope="col">Odsotnost delavca</th>
-                    <th scope="col">NETO montaža</th>
-                    <th scope="col">BRUTO montaža</th>                     
-                </tr>
-                </thead>
-                <tbody> 
-                    {props.data.map((el) => tableRow(el))}
-                    <tr>
-                        <td colspan="2"><b>Skupen neto čas montaže:</b></td>
-                        <td>{props.neto}</td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><b>Skupen bruto čas montaže:</b> </td>
-                        <td>{props.bruto}</td>
-                    </tr>
-                </tbody>
-            </Table><br/>
-            <div className="text-center">
-                <ReactHTMLTableToExcel className="btn btn-danger" table="table" filename="analiza" buttonText="Prenesi"/
-            ></div> 
-        </>
-        
-        );
-    };
     return(
         <>  
             <Button center color="danger" onClick={function(){ handleBody();}}>Prenesi kot XSL</Button>
             <Modal isOpen={modal} toggle={toggle} size="lg">
-                <ModalHeader toggle={toggle}><h2>Podrobnosti</h2></ModalHeader>
+                <ModalHeader toggle={toggle}><h2>Pregled tabele</h2></ModalHeader>
                 <ModalBody>
-                    {modalBody}
+                <Table className="align-items-center table-flush text-center" id="table" responsive >
+                        <thead className="thead-light">
+                            <tr>
+                                <th scope="col">Status</th>
+                                <th scope="col">Šifra</th> 
+                                <th scope="col">Objekt</th>
+                                <th scope="col">Datum</th>
+                                <th scope="col">Avto</th>
+                                <th scope="col">Šofer</th>
+                                <th scope="col">Delavci</th>
+                                <th scope="col">START</th>
+                                <th scope="col">Pričetek</th>
+                                <th scope="col">Konec</th>
+                                <th scope="col">PRIHOD</th>
+                                <th scope="col">NETO čas delavca</th>
+                                <th scope="col">Odsotnost šoferja</th>
+                                <th scope="col">Odsotnost delavca</th>
+                                <th scope="col">NETO montaža</th>
+                                <th scope="col">BRUTO montaža</th>                     
+                            </tr>
+                        </thead>
+                        <tbody> 
+                            {props.data.map((el) => tableRow(el))}
+                            <tr>
+                                <td colspan="2"><b>Skupen neto čas montaže:</b></td>
+                                <td>{props.neto}</td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><b>Skupen bruto čas montaže:</b> </td>
+                                <td>{props.bruto}</td>
+                            </tr>
+                        </tbody>
+                    </Table><br/>
+                    <div className="text-center">
+                       <Button className="btn btn-danger" onClick={() => {excel("table")}}>Prenesi</Button>
+                    </div> 
                 </ModalBody>
                 <ModalFooter>
                     <Button color="danger" onClick={toggle}>Zapri</Button>
                 </ModalFooter>
-            </Modal>
-             
+            </Modal>                           
         </>
 
     );
