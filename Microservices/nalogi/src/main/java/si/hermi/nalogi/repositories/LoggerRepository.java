f090e1bbac602945913d94a64abe18727732b5fa
Ivana P. Lavric <ivana.lavric@student.um.si>
2021-06-14 00:37:51 +0200

    fix: audit be remove

--- Microservices/nalogi/src/main/java/si/hermi/nalogi/repositories/LoggerRepository.java
+++
@@ -1,7 +0,0 @@
-package si.hermi.nalogi.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import si.hermi.nalogi.vao.Logger;
-
-public interface LoggerRepository extends PagingAndSortingRepository<Logger, Integer> {
-}
