a8a26794662120c0825a2deb6314232564cc107c
Tjan <tljubesek@gmail.com>
2021-06-16 00:20:33 +0000

    feat: added second graph

--- Frontend/src/views/Pregled.js
+++ Frontend/src/views/Pregled.js
@@ -52,6 +52,7 @@ const Index = (props) => {
   const [activeNav, setActiveNav] = useState(1);
   const [dataMontazaMesec, setDataMontazaMesec] = useState([0,0,0,0,0,0,0,0,0,0,0,0]);
   const [dataMontazaTeden, setDataMontazaTeden] = useState([0,0,0,0,0,0,0]);
+  const [dataPerMonth, setDataPerMonth] = useState([0,0,0,0,0,0,0,0,0,0,0,0]);
 
 
   React.useEffect(() => {
@@ -63,6 +64,8 @@ const Index = (props) => {
             nalogi.forEach((nalog)=> nalog.ekipe.forEach((ekipa)=>{if(ekipa.datum!=undefined){neto.push({montaza:ekipa.netoMontaza, datum:ekipa.datum})}}));
             handleMontazaTeden(neto);
             handleMontazaMesec(neto);
+
+            handlePerMonth(nalogi);
         }
     });
     setChartExample1Data(data1);
@@ -137,6 +140,30 @@ const handleMontazaTeden = (neto) => {
       setChartExample1Data(data2);
   };
 
+  const handlePerMonth = (nalogi) => {
+    let months = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+    nalogi.forEach((nalog) => {
+      
+      months[new Date(nalog.zacetek).getMonth()]++;
+    });
+
+    setDataPerMonth(nalogPerMonth(months));
+  }
+
+  const nalogPerMonth = (data) => {
+    return {
+      labels: ["Jan", "Feb", "Mar", "Apr", "Maj","Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+      datasets: [
+        {
+          label: "Število",
+          data: data,
+          maxBarThickness: 10,
+        },
+      ],
+    };
+  }
+
   return (
     <>
       <Header />
@@ -196,7 +223,7 @@ const handleMontazaTeden = (neto) => {
               </CardBody>
             </Card>
           </Col>
-          <Col xl="4">
+          <Col xl="4" className="mb-5">
             <Card className="shadow">
               <CardHeader className="bg-transparent">
                 <Row className="align-items-center">
@@ -212,200 +239,13 @@ const handleMontazaTeden = (neto) => {
                 {/* Chart */}
                 <div className="chart">
                   <Bar
-                    data={chartExample2.data}
+                    data={dataPerMonth}
                     options={chartExample2.options}
                   />
                 </div>
               </CardBody>
             </Card>
           </Col>
-        </Row>
-        <Row className="mt-5">
-        <Col xl="4">
-            <Card className="shadow">
-              <CardHeader className="border-0">
-                <Row className="align-items-center">
-                  <div className="col">
-                    <h3 className="mb-0">Napredek ekip</h3>
-                  </div>
-                  <div className="col text-right">
-                    <Button
-                      color="danger"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                      size="sm"
-                    >
-                      Več
-                    </Button>
-                  </div>
-                </Row>
-              </CardHeader>
-              <Table className="align-items-center table-flush" responsive>
-                <thead className="thead-light">
-                  <tr>
-                    <th scope="col">Ime ekipe</th>
-                    <th scope="col">Št. delavcev</th>
-                    <th scope="col" />
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Ekipa 1</th>
-                    <td>4</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">60%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="60"
-                            barClassName="bg-gradient-danger"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Ekipa 2</th>
-                    <td>2</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">70%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="70"
-                            barClassName="bg-gradient-danger"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Ekipa 3</th>
-                    <td>2</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">80%</span>
-                        <div>
-                          <Progress max="100" value="80" barClassName="bg-gradient-danger" />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Ekipa 4</th>
-                    <td>2</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">75%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="75"
-                            barClassName="bg-gradient-danger"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Ekipa 5</th>
-                    <td>4</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">30%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="30"
-                            barClassName="bg-gradient-danger"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Card>
-          </Col>
-          <Col className="mb-5 mb-xl-0" xl="8">
-            <Card className="shadow">
-              <CardHeader className="border-0">
-                <Row className="align-items-center">
-                  <div className="col">
-                    <h3 className="mb-0">Pregled zaposlenih</h3>
-                  </div>
-                  <div className="col text-right">
-                    <Button
-                      color="danger"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                      size="sm"
-                    >
-                      Več
-                    </Button>
-                  </div>
-                </Row>
-              </CardHeader>
-              <Table className="align-items-center table-flush" responsive>
-                <thead className="thead-light">
-                  <tr>
-                    <th scope="col">Zaposleni</th>
-                    <th scope="col">Oddelane ure v mesecu</th>
-                    <th scope="col">Oddelani objekti</th>
-                    <th scope="col">Odstopanje od povprečja</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Delavec 1</th>
-                    <td>150</td>
-                    <td>7</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-success mr-3" /> 46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Delavec 2</th>
-                    <td>180</td>
-                    <td>9</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-warning mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Delavec 3</th>
-                    <td>160</td>
-                    <td>4</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      36,49%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Delavec 4</th>
-                    <td>180</td>
-                    <td>6</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Delavec 5</th>
-                    <td>160</td>
-                    <td>8</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-danger mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Card>
-          </Col>
-          
         </Row>
       </Container>
     </>
